2b5a3c42f28bae8c02721a68490de0b5dcfa3dd4
Aaron Romeo <aaron.romeo@thescore.com>
2017-11-13 07:22:59 -0500

    Restyled with casper (https://github.com/danielbayerlein/middleman-casper).

--- config.rb
+++ config.rb
@@ -1,4 +1,30 @@
 ###
+# middleman-casper configuration
+###
+
+config[:casper] = {
+  blog: {
+    url: 'http://aaronromeo.com',
+    name: '> aaronromeo.com',
+    description: 'Code and other stuff.',
+    date_format: '%d %B %Y',
+    navigation: true,
+    logo: nil
+  },
+  author: {
+    name: 'Aaron Romeo',
+    bio: nil, # Optional
+    location: nil, # Optional
+    website: nil, # Optional
+    gravatar_email: 'aaron.romeo@gmail.com', # Optional
+    twitter: nil # Optional
+  },
+  navigation: {
+    "Home" => "/"
+  }
+}
+
+###
 # Page options, layouts, aliases and proxies
 ###
 
@@ -12,16 +38,48 @@ page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
+
+def get_tags(resource)
+  if resource.data.tags.is_a? String
+    resource.data.tags.split(',').map(&:strip)
+  else
+    resource.data.tags
+  end
+end
+
+def group_lookup(resource, sum)
+  results = Array(get_tags(resource)).map(&:to_s).map(&:to_sym)
+
+  results.each do |k|
+    sum[k] ||= []
+    sum[k] << resource
+  end
+end
+
+tags = resources
+  .select { |resource| resource.data.tags }
+  .each_with_object({}, &method(:group_lookup))
+
+tags.each do |tagname, articles|
+  proxy "/tag/#{tagname.downcase.to_s.parameterize}/feed.xml", '/feed.xml',
+    locals: { tagname: tagname, articles: articles[0..5] }, layout: false
+end
+
+proxy "/author/#{config.casper[:author][:name].parameterize}.html",
+  '/author.html', ignore: true
+
+# General configuration
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
 
 ###
 # Helpers
 ###
-
-set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
@@ -29,8 +87,8 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
+  blog.sources = "articles/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tag/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
@@ -40,21 +98,26 @@ activate :blog do |blog|
   # blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  # blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
+  blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
 
-activate :syntax
+# Pretty URLs - https://middlemanapp.com/advanced/pretty_urls/
+activate :directory_indexes
 
-page "/feed.xml", layout: false
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
+# Middleman-Syntax - https://github.com/middleman/middleman-syntax
+set :haml, { ugly: true }
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true, footnotes: true,
+  link_attributes: { rel: 'nofollow' }, tables: true
+activate :syntax, line_numbers: false
+
+# Middleman-Sprockets - https://github.com/middleman/middleman-sprockets
+activate :sprockets
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -70,4 +133,16 @@ configure :build do
 
   # Minify Javascript on build
   # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Ignoring Files
+  ignore 'javascripts/_*'
+  ignore 'javascripts/vendor/*'
+  ignore 'stylesheets/_*'
+  ignore 'stylesheets/vendor/*'
 end
